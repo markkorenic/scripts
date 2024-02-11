8f4848b66953cc8b26f8c7fdc19c9bacfe3fdcd5
korenix <markkorenic@users.noreply.github.com>
2024-02-11 10:33:13 -0500

    Initial Commit
    simple gas cost calculator based on user input

---
+++ get_gas_cost.py
@@ -0,0 +1,9 @@
+
+#get gallons of gas
+getCost = int(input("how many gallons of gas?: "))
+
+# get cost of gas
+getGallon = float(input("how much is a gallon of gas: "))
+
+#multiply user input ( gallons times cost)
+print(getCost * getGallon)
